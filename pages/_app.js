0e2defe15a81de9e6ecd24c91188101600454d66
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-01-15 15:31:38 +0100

    💄 Add logo and text to tabs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/global.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>WanderGoal - Your Personalized Travel Planner</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
