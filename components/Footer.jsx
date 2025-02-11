b58d584809a0a1a8aedf4e038745e2e2f5181d95
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-02-11 16:15:26 +0100

    💄 Add underlines to Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer className="bg-green-600 text-white">
       <div className="max-w-7xl mx-auto px-4 py-5 flex flex-col items-center space-y-2 sm:space-y-0 sm:flex-row sm:justify-between">
-        <p className="text-sm">
+        <p className="text-sm border-b border-white-200">
           © {new Date().getFullYear()} WanderGoal. All rights reserved.
         </p>
-        <div className="text-sm flex items-center space-x-2">
+        <div className="text-sm flex items-center space-x-2 border-b border-white-200">
           <span>Made with 💚 by</span>
           <a
             href="https://www.instagram.com/ad.fgrd/"
