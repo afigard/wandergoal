02b57ecdf1bb6ca4145671c8cb3b6bdf6314088c
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-02-13 15:44:44 +0100

    💄 One-line Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,21 +4,18 @@ const Footer = () => {
   return (
     <footer className="bg-green-600 text-white">
       <div className="max-w-7xl mx-auto px-4 py-5 flex flex-col items-center space-y-2 sm:space-y-0 sm:flex-row sm:justify-between">
-        <p className="text-sm border-b border-white-200">
-          © {new Date().getFullYear()} WanderGoal. All rights reserved.
-        </p>
-        <div className="text-sm flex items-center space-x-2 border-b border-white-200">
-          <span>Made with 💚 by</span>
+        <p className="text-sm flex items-center">
+          © {new Date().getFullYear()} WanderGoal. All rights reserved. By{" "}
           <a
             href="https://www.instagram.com/ad.fgrd/"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-200 transition"
+            className="ml-1"
             aria-label="Instagram"
           >
             <FaInstagram size={20} />
           </a>
-        </div>
+        </p>
       </div>
     </footer>
   );
