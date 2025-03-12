bf28adae972c42b168f3a8a04e143a23bcc0a515
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-03-12 09:39:59 +0100

    🔥 Remove IG from Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,18 +3,12 @@ import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-green-600 text-white">
-      <div className="max-w-7xl mx-auto px-4 py-5 flex flex-col items-center space-y-2 sm:space-y-0 sm:flex-row sm:justify-between">
+      <div className="max-w-7xl mx-auto px-4 py-5 flex items-center justify-between">
         <p className="text-sm flex items-center">
-          © {new Date().getFullYear()} WanderGoal. All rights reserved. By{" "}
-          <a
-            href="https://www.instagram.com/ad.fgrd/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="ml-1"
-            aria-label="Instagram"
-          >
-            <FaInstagram size={20} />
-          </a>
+          © {new Date().getFullYear()} WanderGoal.
+        </p>
+        <p className="text-sm flex items-center">
+          All rights reserved.
         </p>
       </div>
     </footer>
