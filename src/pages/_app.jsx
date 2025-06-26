d0af8164a23d3f4c98f5d374768264b0f28ff853
Adrien Figard <97508163+afigard@users.noreply.github.com>
2025-06-26 18:37:10 +0200

    ✨ Change favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
         <meta name="author" content="Adrien Figard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="theme-color" content="#4CAF50" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
